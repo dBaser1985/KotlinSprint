b058f1d51be5215d5d1ace47a644fbad77d9426e
dBaser <dbaser@ya.ru>
2025-02-11 00:21:37 +0300

    KS-3-1-string Решение задачи 1 к уроку 3

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val userName = "User"
+    var greeting: String
+
+    greeting = "Добрый день"
+    println("$greeting, $userName!")
+
+    greeting = "Добрый вечер"
+    println("$greeting, $userName!")
+
+}
