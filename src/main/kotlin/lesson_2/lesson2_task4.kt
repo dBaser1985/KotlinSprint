1aa5435e126831beeaeb9882eb75e10ba249a382
dBaser <dbaser@ya.ru>
2025-02-11 21:48:02 +0300

    KS-2-4-fix Исправление решения задачи 4 к уроку 2

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_2
 
-const val BUFF_20_PERCENT = 0.2
+const val DENOMINATOR_100_PERCENT = 100
 
 fun main() {
 
     val crystalOre = 7
     val ironOre = 11
+    val buffPercent = 20
 
-    val bonusCrystalOre = (crystalOre * BUFF_20_PERCENT).toInt()
-    val bonusIronOre = (ironOre * BUFF_20_PERCENT).toInt()
+    val bonusCrystalOre = crystalOre * buffPercent/DENOMINATOR_100_PERCENT
+    val bonusIronOre = ironOre * buffPercent/DENOMINATOR_100_PERCENT
 
     println("Бонусная кристаллическая руда: $bonusCrystalOre")
     println("Бонусная железная руда: $bonusIronOre")
